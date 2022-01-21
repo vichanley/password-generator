d58a5ad04cf9bd4c66c5d8c2953c36b4d860a1e6
vichanley <viccantu23@gmail.com>
2022-01-21 17:25:56 -0600

    Adding character criteria

--- Develop/script.js
+++ Develop/script.js
@@ -8,8 +8,52 @@ if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
 }
 
 else {
-  prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ")
+  prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ");
 }
+
+//Prompting user to choose character types to include in password 
+var lowerChars = confirm("Would you like to include lowercase characters in your password?");
+
+if (!lowerChars) {
+  lowerChars = "";
+}
+
+else {
+  lowerChars = "abcdefghijklmnopqrstuvwxyz";
+}
+
+var upperChars = confirm("Would you like to include uppercase characters in your password?");
+
+if (!upperChars) {
+  upperChars = "";
+}
+
+else {
+  upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+var specialChars = confirm("Would you like to include special characters in your password?");
+
+if (!specialChars) {
+  specialChars = "";
+}
+
+else {
+  specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+}
+
+var numericValues = confirm("Would you like to include numeric values in your password?");
+
+if (!numericValues) {
+  numericValues = "";
+}
+
+else {
+  numericValues = "0123456789";
+}
+
+
+
 
 
 // Get references to the #generate element
