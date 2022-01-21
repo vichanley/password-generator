17b7ee19310510e9d5f90c337244baad6225899c
vichanley <viccantu23@gmail.com>
2022-01-21 17:35:24 -0600

    Validating user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,72 @@
 // Assignment code here
+var passwordCriteria = function () {
 
 //Prompting user to choose length of password 
-var passwordLength = prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ");
+  var passwordLength = prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ");
 
-if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  return generatePassword();
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    return generatePassword();
+  }
+
+  else {
+    prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ");
+  }
+
+  //Prompting user to choose character types to include in password 
+  var lowerChars = confirm("Would you like to include lowercase characters in your password?");
+
+  if (!lowerChars) {
+    lowerChars = "";
+  }
+
+  else {
+    lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  var upperChars = confirm("Would you like to include uppercase characters in your password?");
+
+  if (!upperChars) {
+    upperChars = "";
+  }
+
+  else {
+    upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  var specialChars = confirm("Would you like to include special characters in your password?");
+
+  if (!specialChars) {
+    specialChars = "";
+  }
+
+  else {
+    specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+
+  var numericValues = confirm("Would you like to include numeric values in your password?");
+
+  if (!numericValues) {
+    numericValues = "";
+  }
+
+  else {
+    numericValues = "0123456789";
+  }
+
+
+  //Input validated and ensuring that at least one character type should be selected 
+  var userInput = lowerChars + upperChars + specialChars + numericValues;
+
+  if (userInput.length > 0) {
+    console.log ("User input is " + userInput);
+  }
+
+  else {
+    window.alert("Please select at least one type of character to include in your password.");
+    return passwordCriteria();
+  }
+
 }
-
-else {
-  prompt("Please enter a password length of at least 8 characters and no more than 128 characters.: ");
-}
-
-//Prompting user to choose character types to include in password 
-var lowerChars = confirm("Would you like to include lowercase characters in your password?");
-
-if (!lowerChars) {
-  lowerChars = "";
-}
-
-else {
-  lowerChars = "abcdefghijklmnopqrstuvwxyz";
-}
-
-var upperChars = confirm("Would you like to include uppercase characters in your password?");
-
-if (!upperChars) {
-  upperChars = "";
-}
-
-else {
-  upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-}
-
-var specialChars = confirm("Would you like to include special characters in your password?");
-
-if (!specialChars) {
-  specialChars = "";
-}
-
-else {
-  specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-}
-
-var numericValues = confirm("Would you like to include numeric values in your password?");
-
-if (!numericValues) {
-  numericValues = "";
-}
-
-else {
-  numericValues = "0123456789";
-}
-
-
-
 
 
 // Get references to the #generate element
